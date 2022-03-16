6c2c7cb50d89dedd2c3f1ffe9834f15c73132e1c
Subhasish Das <subhasish@doc32.in>
2022-03-17 00:42:34 +0530

    redux setup basic okaoy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index';
 
 function App() {
+  const account = useSelector((state) => state.account);
+  console.log('account', account);
+
+  const dispatch = useDispatch();
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch,
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {account}
+        <button
+          style={{ fontSize: '2rem', marginTop: '1rem' }}
+          onClick={() => depositMoney(1)}
         >
-          Learn React
-        </a>
+          deposit
+        </button>
+        <button
+          style={{ fontSize: '2rem', marginTop: '1rem' }}
+          onClick={() => withdrawMoney(1)}
+        >
+          withdraw
+        </button>
       </header>
     </div>
   );
